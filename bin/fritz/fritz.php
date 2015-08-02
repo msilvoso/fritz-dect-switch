3591c2660203769be1349a20c2d8ccbda201fc69
Manuel Silvoso <msilvoso@gmail.com>
2015-08-02 19:06:50 +0200

    better respect of PSR-2

--- bin/fritz/fritz.php
+++ bin/fritz/fritz.php
@@ -5,57 +5,65 @@ class fritzbox
     const LOGIN="login_sid.lua";
     const WEBSERVICES="webservices/homeautoswitch.lua?switchcmd=";
 
-
     protected $username;
     protected $password;
     protected $fritzboxUrl;
     protected $ssl=false;
     protected $sessionId=false;
 
-    public function __construct($url,$username,$password) {
+    public function __construct($url, $username, $password) 
+    {
         $this->fritzboxUrl = $url;
         $this->username = $username;
         $this->password = $password;
-	$this->SID = $this->getSessionID();
+        $this->SID = $this->getSessionID();
     }
 
-    public function getSwitchList() {
+    public function getSwitchList() 
+    {
         $url = $this->buildUrl()."/".fritzbox::WEBSERVICES."getswitchlist&sid=".$this->SID;
         return $this->doGetRequest($url);
     }
 
-    public function getSwitchState($ain) {
+    public function getSwitchState($ain) 
+    {
         $url = $this->buildUrl()."/".fritzbox::WEBSERVICES."getswitchstate&ain=$ain&sid=".$this->SID;
         return $this->doGetRequest($url);
     }
 
-    public function setSwitchOff($ain) {
+    public function setSwitchOff($ain) 
+    {
         $url = $this->buildUrl()."/".fritzbox::WEBSERVICES."setswitchoff&ain=$ain&sid=".$this->SID;
         return $this->doGetRequest($url);
     }
 
-    public function setSwitchOn($ain) {
+    public function setSwitchOn($ain) 
+    {
         $url = $this->buildUrl()."/".fritzbox::WEBSERVICES."setswitchon&ain=$ain&sid=".$this->SID;
         return $this->doGetRequest($url);
     }
 
-    public function getSwitchPower($ain) {
+    public function getSwitchPower($ain) 
+    {
         $url = $this->buildUrl()."/".fritzbox::WEBSERVICES."getswitchpower&ain=$ain&sid=".$this->SID;
         return $this->doGetRequest($url);
     }
 
-    public function logout() {
+    public function logout() 
+    {
         $url = $this->buildUrl()."/".fritzbox::LOGIN."?logout=1&sid=".$this->SID;
-	return $this->doGetRequest($url);
+        return $this->doGetRequest($url);
     }
 
-    public function getChallenge() {
+    public function getChallenge() 
+    {
         $url = $this->buildUrl()."/".fritzbox::LOGIN;
         $content = $this->doGetRequest($url);
         return $this->getValueFromXML('CHALLENGE',$content);
     }
 
-    public function getSessionID() {
+    public function getSessionID() 
+    {
         $challenge = $this->getChallenge();
         $response = "$challenge-".md5(mb_convert_encoding($challenge . '-' . $this->password, "UCS-2LE", "UTF-8"));
         $url = $this->buildUrl()."/".fritzbox::LOGIN."?username=".$this->username."&response=".$response;
@@ -63,7 +71,8 @@ class fritzbox
         return $this->getValueFromXML('SID',$content);
     }
 
-    protected function buildUrl() {
+    protected function buildUrl() 
+    {
         if ($this->ssl) {
             $url="https://".$this->fritzboxUrl;
         } else {
@@ -72,7 +81,8 @@ class fritzbox
         return $url;
     }
 
-    protected function getValueFromXML($name, $content) {
+    protected function getValueFromXML($name, $content) 
+    {
         $parser = xml_parser_create();
         $values = array();
         $index = array();
@@ -81,12 +91,12 @@ class fritzbox
         return $values[$index[$name][0]]['value'];
     }
 
-    protected function doGetRequest($url) {
-	$ch = curl_init();
+    protected function doGetRequest($url) 
+    {
+        $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPGET, 1);
-        if ( $this->ssl )
-        {
+        if ( $this->ssl ) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
